c412f63d1d36c99b4cd52bcfe2dd83869d4dd2c3
Narendra Ayer <nkayer@myseneca.ca>
2025-01-23 14:00:41 -0500

    Added lab2c

---
+++ lab2c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Get the arguments from the command line
+name = sys.argv[1]  # First command line argument
+age = int(sys.argv[2])  # Second command line argument, converted to an integer
+
+# Output the formatted message
+print(f"Hi {name}, you are {age} years old.")
+
