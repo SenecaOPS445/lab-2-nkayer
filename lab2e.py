f48185fb05b01ee82a9af908f98938844306de03
Narendra Ayer <nkayer@myseneca.ca>
2025-01-23 14:15:11 -0500

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Author: [Your Full Name]
+# Author ID: [Your Seneca ID]
+# Date Created: 2025/01/23
+
+# Initialize the timer with a value of 10
+timer = 10
+
+# While loop that counts down until timer equals 0
+while timer > 0:
+    print(timer)
+    timer -= 1  # Decrement the timer by 1 each iteration
+
+# Once the countdown is done, print "blast off!"
+print("blast off!")
+
