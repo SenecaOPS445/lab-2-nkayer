12dea51da03acb72df1a21682af673b2ef31bb53
Narendra Ayer <nkayer@myseneca.ca>
2025-01-23 13:44:49 -0500

    Added lab2b

---
+++ lab2b.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Prompt the user for their name and age
+name = input("Name: ")
+age = input("Age: ")
+
+# Print the output with the required format
+print(f"Hi {name}, you are {age} years old.")
+
